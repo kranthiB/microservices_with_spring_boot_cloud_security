f94a95d058a62513d5efb905d4e9b4ce95ecd5bb
kranthiB <kranthi.b76@gmail.com>
2016-08-25 16:56:17 +0530

    Enabled hystrix dashboard and getting the actuator reports without security

--- client-gateway/src/main/java/com/retailstore/ClientGatewayApplication.java
+++ client-gateway/src/main/java/com/retailstore/ClientGatewayApplication.java
@@ -3,11 +3,13 @@ package com.retailstore;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
 @EnableZuulProxy
 @EnableCircuitBreaker
+@EnableHystrixDashboard
 public class ClientGatewayApplication {
 
     public static void main(String[] args) {
