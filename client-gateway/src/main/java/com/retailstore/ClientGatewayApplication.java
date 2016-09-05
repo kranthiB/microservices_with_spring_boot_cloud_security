dbe7db515287ae9b716893cb74afe08370ebcb54
kranthiB <kranthi.b76@gmail.com>
2016-09-05 22:18:54 +0530

    removed security for management related urls

--- client-gateway/src/main/java/com/retailstore/ClientGatewayApplication.java
+++ client-gateway/src/main/java/com/retailstore/ClientGatewayApplication.java
@@ -3,12 +3,13 @@ package com.retailstore;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
 @EnableZuulProxy
-@EnableCircuitBreaker
+@EnableHystrix
 @EnableHystrixDashboard
 public class ClientGatewayApplication {
 
--- product-catalogue-service/src/main/java/com/retailstore/ProductCatalogueServiceApplication.java
+++ product-catalogue-service/src/main/java/com/retailstore/ProductCatalogueServiceApplication.java
@@ -3,11 +3,13 @@ package com.retailstore;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
 @EnableResourceServer
 @EnableDiscoveryClient
+@EnableHystrix
 public class ProductCatalogueServiceApplication {
 
     public static void main(String[] args) {
